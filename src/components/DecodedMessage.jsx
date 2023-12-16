78b4aa7625f11e693c8bfe7ea91f6cb534cd1c25
simbld <s_bullado51@hotmail.com>
2023-12-16 09:46:45 +0100

    Definition of the function to decode the hidden message

--- src/components/DecodedMessage.jsx
+++ src/components/DecodedMessage.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
+import { hiddenMessage } from "../modules/CodedMessage";
 
 function DecodedMessage() {
-  const [decodedMessage, setDecodedMessage] = useState("");
+  const [isDecodedMessage, setIsDecodedMessage] = useState("");
+  const decoder = hiddenMessage
+    .filter((char) => char !== "X" && char !== ",")
+    .join("");
 
+  console.log(decoder);
   return (
     <>
       <div>
         <h1>Decode Messages 1 :</h1>
 
-        <div>Message is : {decodedMessage}</div>
+        <div>Message is : {isDecodedMessage}</div>
       </div>
     </>
   );
