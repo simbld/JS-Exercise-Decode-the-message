f91a2b60c9160202b22d8239e14b7c7dc8e77938
simbld <s_bullado51@hotmail.com>
2023-12-18 05:05:34 +0100

    Add a timer to display the decoded message

--- src/components/DecodedMessage.jsx
+++ src/components/DecodedMessage.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { hiddenMessage } from "../modules/CodedMessage";
 
 function DecodedMessage() {
   const [isDecodedMessage, setIsDecodedMessage] = useState("");
-
-  const toggleDecodeButton = () => {
-    if (isDecodedMessage === "") {
-      const decoder = hiddenMessage
-        .filter((char) => char !== "X" && char !== ",")
-        .join("");
-      setIsDecodedMessage(decoder);
-    } else {
-      setIsDecodedMessage("");
+  useEffect(() => {
+    let timer;
+    if (isDecodedMessage !== "") {
+      timer = setTimeout(() => {
+        setIsDecodedMessage("");
+      }, 1000);
     }
+    return () => clearTimeout(timer);
+  }, [isDecodedMessage]);
+  const decodeButton = () => {
+    const decoder = hiddenMessage
+      .filter((char) => char !== "X" && char !== ",")
+      .join("");
+    setIsDecodedMessage(decoder);
   };
 
   return (
@@ -20,13 +24,20 @@ function DecodedMessage() {
       <div className="container">
         <div className="safe">
           <div className="button-container">
-            <button className="button" onClick={toggleDecodeButton}>
-              press to decode
-            </button>
+            <button className="button button-choice">Message</button>
+            <button className="button button-choice">Code</button>
           </div>
           <div className="safe-door">
             <div className="codedmessage">"{hiddenMessage.join(`", "`)}"</div>
-            <div className="safe-content">{isDecodedMessage}</div>
+            <div className="content-container">
+              <div className="safe-content">{isDecodedMessage}</div>
+            </div>
+          </div>
+
+          <div className="decode-container">
+            <button className="button" onClick={decodeButton}>
+              press to decode
+            </button>
           </div>
         </div>
       </div>
