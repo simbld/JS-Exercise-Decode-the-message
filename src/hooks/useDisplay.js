fe9b9afe1ac4de2c2b54d9ebe7fbc6f8ff3d48f4
simbld <s_bullado51@hotmail.com>
2023-12-21 09:15:42 +0100

    Add jsdoc

--- src/hooks/useDisplay.js
+++ src/hooks/useDisplay.js
@@ -1,3 +1,18 @@
+/**
+ * @param {any} initialMessage - Le message initial à afficher.
+ * Ce paramètre définit l'état initial de currentMessage.
+ * @returns {[any, Function]} Un tableau contenant deux éléments :
+ *          - currentMessage : Le message actuellement affiché.
+ *          - switchMessage : Une fonction pour changer le message actuel.
+ *
+ * @example
+ * // Pour le composant qui importera useDisplay :
+ * const [currentMessage, switchMessage] = useDisplay(hiddenMessage);
+ *
+ * // Pour changer le message :
+ * switchMessage("hiddenMessage");
+ */
+
 import { useState } from "react";
 
 const useDisplay = (initialMessage) => {
